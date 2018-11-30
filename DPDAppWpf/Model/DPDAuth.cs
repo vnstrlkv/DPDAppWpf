262fd84376546a6f65b54b8673a832cc3ddf7912
vnstrlkv <vnstrlkv@gmail.com>
2018-11-30 13:58:54 +0700

    1

---
+++ DPDAppWpf/Model/DPDAuth.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DPDAppWpf.DPDCalc;
+using DPDAppWpf.DPDGeo;
+using DPDAppWpf.DPDOrder;
+
+namespace DPDAppWpf.Model
+{
+   public abstract class Auth 
+    {
+        public void AuthSet(DPDAppWpf.DPDCalc.auth a, long ClientNumber, string Key)
+        {
+            a.clientKey = Key;
+            a.clientNumber = ClientNumber;
+        }
+
+        public void AuthSet(DPDAppWpf.DPDCalc.auth a, string Key, long ClientNumber)
+        {
+            a.clientKey = Key;
+            a.clientNumber = ClientNumber;
+        }
+
+    }
+}
---
+++ DPDAppWpf/Model/DPDGeography.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DPDAppWpf.DPDGeo;
+
+
+namespace DPDAppWpf.Model
+{
+    public class DPDGeography
+    {
+        private auth Auth;
+        public void AuthSet(long ClientNumber, string Key)
+        {
+            Auth.clientKey = Key;
+            Auth.clientNumber = ClientNumber;
+        }
+
+        public void AuthSet(string Key, long ClientNumber)
+        {
+            Auth.clientKey = Key;
+            Auth.clientNumber = ClientNumber;
+        }
+
+        public dpdCitiesCashPayRequest GeoRequest;
+
+        public void SetGeoRequest(string CountryCode)
+        {
+            GeoRequest.auth = Auth;
+            GeoRequest.countryCode = CountryCode;
+        }
+
+
+
+    }
+}
